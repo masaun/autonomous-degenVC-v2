22b7a184f5e91547a66a1d293a2b6fb055fc8f31
masaun <masanoriuno@gmail.com>
2021-04-30 11:11:46 +0900

    [SC]: Define 3 methods for executing 3 process to be automated in the AutonomousDegenVC.sol

--- contracts/AutonomousDegenVC.sol
+++ contracts/AutonomousDegenVC.sol
@@ -11,8 +11,21 @@ pragma solidity 0.7.4;
  */
 contract AutonomousDegenVC {
 
-    constructor() public {
-        /// [Todo]:
-    }
+    constructor() public {}
+
+    /**
+     * @notice - ① A Uniswap market is created for the new project
+     */
+    function createUniswapMarketForProject() public returns (bool) {}
+
+    /**
+     * @notice - Part of the tokens supply is Alphadropped (airdropped) to wallets that hold our $DGVC UNI-V2 LP tokens in proportion to their share of the LP;
+     */    
+    function alphadropPartOfTokens() public returns (bool) {}
+
+    /**
+     * @notice - ③ A Liquid Vault is capitalized with project tokens to incentivise "early liquidity" 
+     */
+    function capitalizeWithProjectTokens() public returns (bool) {}
 
 }
