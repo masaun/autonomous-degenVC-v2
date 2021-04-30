ef1911fc03a4a23d3572a1e2236ee5af2188ae70
masaun <masanoriuno@gmail.com>
2021-05-01 08:46:32 +0900

    [SC]: Inherit burn() from IProjectToken.sol to ProjectToken.sol

--- contracts/ProjectToken.sol
+++ contracts/ProjectToken.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.7.4;
 
+import { IProjectToken } from "./IProjectToken.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract ProjectToken is ERC20 {
+contract ProjectToken is IProjectToken, ERC20 {
     constructor(
         string memory name, 
         string memory symbol, 
@@ -12,7 +13,7 @@ contract ProjectToken is ERC20 {
         _mint(msg.sender, initialSupply);
     }
 
-    function burn(uint amount) public returns (bool) {
+    function burn(uint amount) external override returns (bool) {
         _burn(msg.sender, amount);
     }
 
