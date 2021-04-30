cb31c629a6e68269faa9e5c96db1491ce8971805
masaun <masanoriuno@gmail.com>
2021-04-30 10:54:54 +0900

    [SC]: Define AutonomousDegenVC.sol

---
+++ contracts/AutonomousDegenVC.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.7.4;
+
+/**
+ * @notice - This is a smart contract that is automate process of Degen.VC
+ */
+contract AutonomousDegenVC {
+
+    constructor() public {
+        /// [Todo]:
+    }
+
+}
