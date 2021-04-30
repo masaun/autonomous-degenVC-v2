77fdf2f229a4add213919a175b735521bbffa701
masaun <masanoriuno@gmail.com>
2021-04-30 10:57:55 +0900

    [SC]: Add comments of 3 process to be automated

--- contracts/AutonomousDegenVC.sol
+++ contracts/AutonomousDegenVC.sol
@@ -3,6 +3,11 @@ pragma solidity 0.7.4;
 
 /**
  * @notice - This is a smart contract that is automate process of Degen.VC
+ *
+ * ① A Uniswap market is created for the new project
+ * ② Part of the tokens supply is Alphadropped (airdropped) to wallets that hold our $DGVC UNI-V2 LP tokens in proportion to their share of the LP;
+ * ③ A Liquid Vault is capitalized with project tokens to incentivise "early liquidity" 
+ *
  */
 contract AutonomousDegenVC {
 
