547568b4c690ea9f6ceb1d909b896fe2bd64d752
masaun <masanoriuno@gmail.com>
2021-04-30 16:50:46 +0900

    [SC]: Define ProjectTokenFactory.sol and LiquidVaultFactory.sol

---
+++ contracts/LiquidVaultFactory.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.7.4;
+
+import { LiquidVault } from "./degen-vc/LiquidVault.sol";
+
+contract LiquidVaultFactory {
+
+    constructor() public {}
+
+    function createLiquidVault() public returns (bool) {}
+
+}
